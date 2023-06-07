74b93eb0d8bc4d0c6083d77c53cc5782317514cb
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-06-07 10:04:49 -0300

    finalizando parte visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Header } from './components/Header/Header';
 import { Sidebar } from './components/Sidebar/Sidebar';
+import { Post } from './components/Post/Post';
+
 import './global.css';
 
 import styles from './App.module.css';
@@ -12,7 +14,11 @@ function App() {
 
      <div className={styles.wrapper}>
        <Sidebar />
-       <main></main>
+       <main>
+         <Post />
+         <Post />
+         <Post />
+       </main>
      </div>
     </>
   )
---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return(
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://github.com/bruno-rodrigues-d.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Bruno Rodrigues</strong>
+              <time title="06 de Junho às 16:59h" dateTime="2023-06-06 16:59:00">Cerca de 2h atrás</time>
+            </div>
+
+            <button title='Deletar comntário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Bruno, parabéns!! 🚀</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
