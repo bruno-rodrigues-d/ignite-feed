7307106771c715ed4ac12a1af5a2add1f93459b0
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-06-07 10:58:42 -0300

    Aplicando responsividade e criando o componente Avatar

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../Avatar/Avatar';
 import { Comment } from '../Comment/Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/bruno-rodrigues-d.png" alt="" />
+          <Avatar src="https://github.com/bruno-rodrigues-d.png" />
           <div className={styles.authorInfo}>
             <strong>Bruno Rodrigues</strong>
             <span>Web Developer</span>
