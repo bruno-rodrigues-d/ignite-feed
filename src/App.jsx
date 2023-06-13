851a8c1204eb436c8cf8bdb57f258c7a94039360
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-06-13 09:06:09 -0300

    recebendo os valores do post e populando as informações

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import { Header } from './components/Header/Header';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Post } from './components/Post/Post';
+import styles from './App.module.css';
 
 import './global.css';
 
-import styles from './App.module.css';
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/bruno-rodrigues-d.png',
+      name: 'Bruno Rodrigues',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-06-07 11:15:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/bruno-rodrigues-d.png',
+      name: 'Bruno Rodrigues',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-06-05 11:15:00'),
+  },
+];
 
 function App() {
 
@@ -15,9 +45,15 @@ function App() {
      <div className={styles.wrapper}>
        <Sidebar />
        <main>
-         <Post />
-         <Post />
-         <Post />
+         {posts.map(post => {
+           return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+           )
+         })}
        </main>
      </div>
     </>
